af41c52b18be90de05bc839bce98e99deee8fcbe
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-03-03 23:44:27 -0800

    math challenge

---
+++ math_challenge.js
@@ -0,0 +1,14 @@
+/* number challenge */
+
+let x = Math.floor(Math.random() * 100 + 1)
+let y = Math.floor(Math.random() * 50 + 1)
+
+let sum = x + y
+let difference = x - y
+let product = x * y
+let quotient = x / y
+let remainder = x % y
+
+console.log(`x: ${x}, y: ${y}`)
+
+console.log(`sum: ${sum}, difference: ${difference}, product: ${product}, quotient: ${quotient}, remainder: ${remainder}`)
