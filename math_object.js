68b916fb7742de8ecf872fd6d5c563730da70874
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-03-03 23:36:46 -0800

    math object

---
+++ math_object.js
@@ -0,0 +1,37 @@
+let x;
+
+x = Math.sqrt(9);
+console.log(x)
+
+//gives us the absolute number (removes any negative sign)
+x = Math.abs(9);
+console.log(x)
+
+x = Math.round(9.9);
+console.log(x)
+
+x = Math.ceil(4.2);
+console.log(x)
+
+x = Math.floor(9.4);
+console.log(x)
+
+//to the power of
+x = Math.pow(2, 3);
+console.log(x)
+
+
+x = Math.min(4, 5);
+console.log(x)
+
+x = Math.max(4, 5);
+console.log(x)
+
+x = Math.random()
+console.log(x)
+
+let y = Math.random() * 10 + 1
+console.log(y)
+
+y = Math.floor(Math.random() * 10 + 1)
+console.log(y)
